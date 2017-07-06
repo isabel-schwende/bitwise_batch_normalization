581a9f9991915bac2c718d5315b97b72a4c6fb0f
isabel-schwende <isabelschwende@yahoo.de>
2017-07-06 12:54:24 +0200

    Add randomization of gamma and beta

--- main_test.cpp
+++ main_test.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include <random>
 // copied the Eigen folder to /usr/local/include/ or put symlink
+
+// run this program as
+// g++ main_test.cpp -std=c++11 -o test
 using namespace Eigen;
 using namespace std;
 int main()
@@ -11,9 +15,16 @@ int main()
   m = (m + MatrixXd::Constant(3,3,1.0)) * 50;
   MatrixXi src = m.cast <int> ();
   cout << "src =" << endl << src << endl;
+  
+  mt19937 rng;
+  rng.seed(random_device()());
+  uniform_int_distribution<mt19937::result_type> dist8(1,8);
+  uniform_int_distribution<mt19937::result_type> dist_beta(1,128);
 
   // gamma has to be a power of 2 (or could apply highest_power_of2)
-  int gamma = 8;
+  int gamma = dist8(rng);
   // can be any integer
-  int beta =  5;
+  int beta =  dist_beta(rng);
+  cout << "gamma =" << endl << gamma << endl;
+  cout << "beta =" << endl << beta << endl;
 }
