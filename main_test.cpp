980f2c60fb080bd02dbfa5d41cbd896c74dd9021
isabel-schwende <isabelschwende@yahoo.de>
2017-07-06 13:16:28 +0200

    Add comments and TODO tasks

--- main_test.cpp
+++ main_test.cpp
@@ -9,8 +9,12 @@ using namespace Eigen;
 using namespace std;
 int main()
 {
+  // TODO: extend for multiple channel inputs
+
   // generate random input matrix of integers 
   // output of convolutional layer (assuming batch norm after conv and before activation)
+  // TODO: set matrix size in flexible manner
+  // TODO: randomization to constants
   MatrixXd m = MatrixXd::Random(3,3);
   m = (m + MatrixXd::Constant(3,3,1.0)) * 50;
   MatrixXi src = m.cast <int> ();
@@ -21,10 +25,15 @@ int main()
   uniform_int_distribution<mt19937::result_type> dist8(1,8);
   uniform_int_distribution<mt19937::result_type> dist_beta(1,128);
 
-  // gamma has to be a power of 2 (or could apply highest_power_of2)
+  // gamma has to be a power of 2 
+  // in this case we'll just generate the power of two as an integer without computing it yet
   int gamma = dist8(rng);
   // can be any integer
   int beta =  dist_beta(rng);
   cout << "gamma =" << endl << gamma << endl;
   cout << "beta =" << endl << beta << endl;
+
+  // init running mean randomly
+
+  // init variance randomly
 }
