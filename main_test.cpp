fdfd7405e9abf257fe09f0052c8bc2d39b1085dd
isabel-schwende <isabelschwende@yahoo.de>
2017-07-06 11:11:30 +0200

    add Eigen library and get data init to work

--- main_test.cpp
+++ main_test.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <Eigen/Dense>
+// copied the Eigen folder to /usr/local/include/ or put symlink
 using namespace Eigen;
 using namespace std;
 int main()
 {
+  // generate random input matrix of integers 
+  // output of convolutional layer (assuming batch norm after conv and before activation)
   MatrixXd m = MatrixXd::Random(3,3);
-  m = (m + MatrixXd::Constant(3,3,1.2)) * 50;
-  cout << "m =" << endl << m << endl;
-  VectorXd v(3);
-  v << 1, 2, 3;
-  cout << "m * v =" << endl << m * v << endl;
+  m = (m + MatrixXd::Constant(3,3,1.0)) * 50;
+  MatrixXi src = m.cast <int> ();
+  cout << "src =" << endl << src << endl;
+
+  // gamma has to be a power of 2 (or could apply highest_power_of2)
+  int gamma = 8;
+  // can be any integer
+  int beta =  5;
 }
