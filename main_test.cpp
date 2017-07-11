21d0a0679e249f9bfb4b1014a4327202b259c5af
isabel-schwende <isabelschwende@yahoo.de>
2017-07-11 12:25:53 +0200

    Add running variance, epsilon and output init

--- main_test.cpp
+++ main_test.cpp
@@ -10,11 +10,13 @@ using namespace std;
 int main()
 {
   // TODO: extend for multiple channel inputs -> would need Tensor instead of Matrix object
+  // but this can be ignored for the theoretical tests
   
   const int num_rows = 5;
   const int num_cols = 5;
   // generate random input matrix of integers 
   // output of convolutional layer (assuming batch norm after conv and before activation)
+  // convolution -> batch normalization -> activation -> quantization
 
   MatrixXd rand_floats = MatrixXd::Random(num_rows,num_cols);
   rand_floats = (rand_floats + MatrixXd::Constant(num_rows,num_cols,1.0)) * 50;
@@ -25,6 +27,7 @@ int main()
   rng.seed(random_device()());
   uniform_int_distribution<mt19937::result_type> dist8(1,8);
   uniform_int_distribution<mt19937::result_type> dist_beta(1,128);
+  uniform_int_distribution<mt19937::result_type> dist_var(1,10);
 
   // gamma has to be a power of 2 
   // in this case we'll just generate the power of two as an integer without computing it yet
@@ -34,10 +37,19 @@ int main()
   cout << "gamma =" << endl << gamma << endl;
   cout << "beta =" << endl << beta << endl;
 
-  // init running mean randomly
+  // init running mean as mean of target but could be also randomly initalized
   float float_mean = target.mean();
   int running_mean = (int)float_mean;
+  cout << "running mean =" << endl << running_mean << endl;
 
   // init variance randomly
+  int running_var =  dist_var(rng);
+  cout << "running variance =" << endl << running_var << endl;
+
+  // constant epsilon to prevent division by zero
+  const int eps = 1;
+
+  // init output matrix
+  MatrixXi output = MatrixXi::Zero(num_rows,num_cols);
   
 }
